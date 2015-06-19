810128114f0263667a60810767c84415938b3234
Steve Castle <steve.castle@drssmail.com>
2015-06-19 08:51:24 -0700

    Added Inject to DEV dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
@@ -7,6 +7,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var inject = require('inject');
 
 // Lint Task
 gulp.task('lint', function() {
